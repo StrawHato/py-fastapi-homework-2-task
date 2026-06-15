53e01bf414230cdd5ab06912af3eb7d4caf321f7
Maksym Pakula <gargantadoto@gmail.com>
2026-06-15 16:38:06 +0200

    add schemas for movie, country, actors/genres/languages models

--- src/schemas/movies.py
+++ src/schemas/movies.py
@@ -1,1 +1,78 @@
-# Write your code here
+from typing import List, Optional
+
+from pydantic import BaseModel
+from datetime import date
+
+
+class CountryBase(BaseModel):
+    code: str
+    name: str
+
+
+class CountryCreate(CountryBase):
+    pass
+
+
+class CountryRead(CountryBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+
+class GALBase(BaseModel):
+    name: str
+
+
+class GALCreate(GALBase):
+    pass
+
+
+class GALRead(GALBase):
+    id: int
+
+    class Config:
+        from_attributes = True
+
+
+class MovieBase(BaseModel):
+    name: str
+    date: date
+    score: float
+    overview: str
+    status: str
+    budget: float
+    revenue: float
+
+
+class MovieCreate(MovieBase):
+    country: str
+    genres: List[str]
+    actors: List[str]
+    languages: List[str]
+
+
+class MovieUpdate(MovieBase):
+    pass
+
+
+class MovieDetailRead(MovieBase):
+    id: int
+    country: CountryRead
+    genres: List[GALRead]
+    actors: List[GALRead]
+    languages: List[GALRead]
+
+    class Config:
+        from_attributes = True
+
+
+class MovieListRead(BaseModel):
+    movies: List[MovieDetailRead]
+    prev_page: Optional[str] = None
+    next_page: Optional[str] = None
+    total_pages: int
+    total_items: int
+
+    class Config:
+        from_attributes = True
